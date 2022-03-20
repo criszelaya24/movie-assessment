45fcaf6cada00faecf5dfd87da12b3419b4b8e76
Cristopher Palacios <criszelaya24@hotmail.com>
2022-03-19 21:21:01 -0600

    Creating express app and run file to start it

---
+++ backend/src/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Response, Request } from 'express';
+import AppRouter from './AppRouter';
+import './controllers/RootController';
+import cookieSession from 'cookie-session';
+const app = express();
+
+app.use(express.json());
+app.use(cookieSession({ keys: [ 'expressType' ] }));
+app.use(AppRouter.getInstance());
+
+app.all('*', (req:Request, res:Response) => {
+    res.status(404).send({
+        error: 'Route not found',
+    });
+});
+
+export default app;
