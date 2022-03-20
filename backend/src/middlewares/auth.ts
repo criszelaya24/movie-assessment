9bbc2c2168243f6650be5e3d2dfb68819219005a
Cristopher Palacios <criszelaya24@hotmail.com>
2022-03-20 01:59:35 -0600

    Adding auth middleware to check token user

---
+++ backend/src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { default  as jwt }  from 'jsonwebtoken';
+import { Response, NextFunction } from 'express';
+import { RequestWithHeaders } from '../interfaces/Request';
+import User from '../models/User';
+import { Token } from '../interfaces/DatabaseModels';
+import sendError from '../utils/sendError';
+
+const verify = (token:string):Token => {
+    return jwt.verify(token, process.env.SECRET_WORD) as Token;
+};
+
+const auth = async (req:RequestWithHeaders, res:Response, next:NextFunction) => {
+    try {
+        const token:string = req.header('Authorization')?.replace('Bearer ', '');
+
+        if (!token) throw { message: 'Token is required', code: 401 };
+
+        const decoded = verify(token);
+        const user = await User.findOne({ '_id': decoded._id, 'tokens.token': token });
+
+        if (!user) throw { message: 'User not found', code: 404 };
+
+        req.token = token;
+        req.user = user;
+        next();
+    } catch (error) {
+        sendError(res, error);
+    }
+};
+
+export default auth;
