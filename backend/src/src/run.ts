97772604160f36a9fbd0dea7520a042152e7a663
Cristopher Palacios <criszelaya24@hotmail.com>
2022-03-19 21:21:43 -0600

    Modify message from start app

--- backend/src/src/run.ts
+++ backend/src/src/run.ts
@@ -1,4 +1,4 @@
 import app from './app';
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`app listening on port ${port}!`));
