b79dc8f9f16e2551ab11448c363e4d19df004a63
Cristopher Palacios <criszelaya24@hotmail.com>
2022-03-19 21:16:01 -0600

    creating interface for request and descriptor from decorator

---
+++ backend/src/src/interfaces/Request.ts
@@ -0,0 +1,8 @@
+import { Request, RequestHandler } from 'express';
+export interface RequestWithBody extends Request {
+    body: { [key: string]: string | undefined },
+}
+
+export interface RouteHandlerDescriptor {
+    value?: RequestHandler
+}
