fd3a403afeaa8b4fab1254447fa9ec50a46200ec
Cristopher Palacios <criszelaya24@hotmail.com>
2022-03-20 02:00:42 -0600

    Adding login and logout and using handle error function

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,23 +1,50 @@
 import { Response } from 'express';
-import { RequestWithBody } from '../interfaces/Request';
-import { post } from './decorators/routes';
+import { RequestWithBody, RequestWithHeaders } from '../interfaces/Request';
+import { post, del } from './decorators/routes';
 import { controller } from './decorators/controller';
+import { use } from './decorators/use';
 import User from '../models/User';
+import auth from '../middlewares/auth';
+import sendError from '../utils/sendError';
 @controller('/users')
 export class UserController {
 
-    @post('')
+    @post('/register')
     async register(req:RequestWithBody, res:Response):Promise<any> {
-        const user = new User(req.body);
-
         try {
+            const user = new User(req.body);
+
             await user.save();
             const token = await user.generateAuthToken();
 
             res.status(201).send({ user, token });
         } catch (e) {
-            console.log('Error:', e);
+            return sendError(res, e);
         }
+    }
+
+    @post('/login')
+    async postLogin(req:RequestWithBody, res:Response):Promise<any> {
+        try {
+            const user = await User.findByCredentials(req.body.email, req.body.password);
+
+            const token = await user.generateAuthToken();
+
+            res.send({ user, token });
+        } catch (e) {
+            sendError(res, e);
+        }
+    }
+
+    @del('/logout')
+    @use(auth)
+    async logout(req:RequestWithHeaders, res:Response):Promise<any> {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+        await req.user.save();
+
+        res.send({ data: true });
     }
 
 }
--- backend/src/interfaces/Request.ts
+++ backend/src/interfaces/Request.ts
@@ -1,6 +1,12 @@
 import { Request, RequestHandler } from 'express';
+import { IUserDocument as User } from '../interfaces/DatabaseModels';
 export interface RequestWithBody extends Request {
     body: { [key: string]: string | undefined },
+}
+
+export interface RequestWithHeaders extends Request {
+    user: User,
+    token: string
 }
 
 export interface RouteHandlerDescriptor {
