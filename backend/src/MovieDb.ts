cf5afa14c866ce785de88010cba97ef41f7f50bb
Cristopher Palacios <criszelaya24@hotmail.com>
2022-03-21 14:30:08 -0600

    Skiping header from req and adding interface method to class

--- backend/src/MovieDb.ts
+++ backend/src/MovieDb.ts
@@ -1,5 +1,5 @@
 import { default as MovieDbClient } from 'node-themoviedb';
-import { BodyMoviesDb, MovieDbClass, NowPlaying } from './interfaces/MovieClient';
+import { BodyMoviesDb, MovieDbClass, NowPlaying, Detail } from './interfaces/MovieClient';
 
 export default class MovieDb implements MovieDbClass {
 
@@ -9,7 +9,7 @@ export default class MovieDb implements MovieDbClass {
         this.movieDbClient = new MovieDbClient(process.env.MOVIE_API_KEY);
     }
 
-    async getNowPlaying({ page, region }:BodyMoviesDb) {
+    async getNowPlaying({ page, region }:BodyMoviesDb):Promise<NowPlaying> {
         const nowPlaying =  await this.movieDbClient.movie.getNowPlaying({
             query: { page, region },
         });
@@ -17,14 +17,14 @@ export default class MovieDb implements MovieDbClass {
         return nowPlaying as NowPlaying;
     }
 
-    async movieDetail(id:number) {
+    async movieDetail(id:number):Promise<Detail> {
         const movie = await this.movieDbClient.movie.getDetails({
             pathParameters: {
                 movie_id: id,
             },
         });
 
-        return movie;
+        return { ...movie.data };
     }
 
 }
--- backend/src/interfaces/MovieClient.ts
+++ backend/src/interfaces/MovieClient.ts
@@ -7,7 +7,13 @@ export interface NowPlaying {
     data: any
 }
 
+export interface Detail {
+    [key:string]: any
+}
+
 export interface MovieDbClass {
     // eslint-disable-next-line no-unused-vars
     getNowPlaying(params:BodyMoviesDb): Promise<NowPlaying>
+    // eslint-disable-next-line no-unused-vars
+    movieDetail(id:number): Promise<Detail>
 }
