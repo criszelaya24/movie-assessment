8bae82329619b04e2c9c5e1900647ac40e0bd60c
Cristopher Palacios <criszelaya24@hotmail.com>
2022-03-21 12:26:39 -0600

    Creating interface for adding countryInfo req

--- backend/src/interfaces/Request.ts
+++ backend/src/interfaces/Request.ts
@@ -1,12 +1,18 @@
 import { Request, RequestHandler } from 'express';
 import { IUserDocument as User } from '../interfaces/DatabaseModels';
+import { CountryInfo } from './country';
 export interface RequestWithBody extends Request {
     body: { [key: string]: string | undefined },
 }
 
 export interface RequestWithHeaders extends Request {
     user: User,
-    token: string
+    token: string,
+    headers: {
+        [key:string]: string
+    },
+    ipInfo: string,
+    countryInfo: CountryInfo
 }
 
 export interface RouteHandlerDescriptor {
