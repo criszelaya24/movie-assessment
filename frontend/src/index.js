07d0bf965ca52fca35c5dc3ab483b087801e9391
Cristopher Palacios <criszelaya24@hotmail.com>
2022-03-23 14:06:12 -0600

    Adding provider to redux and using redux thunk to implement reducers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import authReducer from './store/reducers/auth';
+const rootReducer = combineReducers({
+  authReducer,
+});
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(thunk)
+))
+
+const app = (
+  <Provider store={store}>
+      <BrowserRouter>
+          <App/>
+      </BrowserRouter>
+  </Provider>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  app,
   document.getElementById('root')
 );
 
